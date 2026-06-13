cb45aa65d7f3fa998c92df362d5f79ef4e1ed887
Arif Febriansyah <arif.febr@gmail.com>
2026-06-13 13:24:17 +0700

    Fix: Make PropertyChanged event nullable to match INotifyPropertyChanged interface

--- ThreeFingerDragOnWindows/settings/ThreeFingerDragSettings.xaml.cs
+++ ThreeFingerDragOnWindows/settings/ThreeFingerDragSettings.xaml.cs
@@ -21,7 +21,7 @@ public sealed partial class ThreeFingerDragSettings : INotifyPropertyChanged{
     }
 
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    public event PropertyChangedEventHandler? PropertyChanged;
 
     private void OnPropertyChanged(string propertyName){
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
